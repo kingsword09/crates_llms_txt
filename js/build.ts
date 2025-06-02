9dde217a8927ab1b416ab466a47d283ea9e64898
Kingsword <kingsword09@gmail.com>
2025-06-02 21:18:57 +0800

    feat(js): add JavaScript package build and configuration

---
+++ js/build.ts
@@ -0,0 +1,25 @@
+import { build } from "jsr:@deno/dnt@^0.42.1";
+import denoJson from "./deno.json" with { type: "json" };
+
+if (import.meta.main) {
+  await build({
+    entryPoints: ["./mod.ts"],
+    outDir: ".npm",
+    shims: { deno: true },
+    declaration: "separate",
+    package: {
+      name: "crates-llms-txt",
+      version: denoJson.version,
+      description: "A repository for generating content for llms.txt and llms-full.txt files used by Rust libraries.",
+      license: "MIT",
+      author: "Kingsword kingsword09 <kingsword09@gmail.com>",
+      homepage: "https://github.com/kingsword09/crates_llms_txt#readme",
+      bugs: { url: "https://github.com/kingsword09/crates_llms_txt/issues" },
+      repository: { type: "git", url: "git+https://github.com/kingsword09/crates_llms_txt.git" },
+    },
+    postBuild() {
+      Deno.copyFileSync("../LICENSE", ".npm/LICENSE");
+      Deno.copyFileSync("../lib/README.md", ".npm/README.md");
+    },
+  });
+}
---
+++ js/mod.ts
@@ -0,0 +1,44 @@
+import { get_llms_config } from "./crates_llms_txt.js";
+
+type SessionItem = { title: string; description: string; link: string; };
+
+type FullSessionItem = { content: string; link: string; };
+
+type LLMsStandardStringConfig = { lib_name: string; version: string; sessions: string; full_sessions: string; };
+
+/**
+ * The standard configuration for LLMs
+ */
+export type LLMsStandardConfig = {
+  libName: string;
+  version: string;
+  sessions: SessionItem[];
+  fullSessions: FullSessionItem[];
+};
+
+/**
+ * Get the standard configuration for LLMs based on library name and version
+ *
+ * @param lib_name - The name of the library
+ * @param version - The version of the library
+ * @returns LLMsStandardConfig - The standard configuration for LLMs
+ *
+ * @module
+ *
+ * @example
+ * ```ts
+ * import { get_llms_standard_config } from "jsr:@llms/llms@^0.1.0";
+ *
+ * const llmsStandardConfig = await get_llms_standard_config("clap", "4.5.39");
+ * ```
+ */
+export const get_llms_standard_config = async (lib_name: string, version?: string): Promise<LLMsStandardConfig> => {
+  const config: LLMsStandardStringConfig = await get_llms_config(lib_name, version);
+
+  return {
+    libName: config.lib_name,
+    version: config.version,
+    sessions: JSON.parse(config.sessions),
+    fullSessions: JSON.parse(config.full_sessions),
+  } satisfies LLMsStandardConfig;
+};
